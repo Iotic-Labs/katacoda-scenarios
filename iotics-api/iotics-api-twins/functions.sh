0af26b8cd1ffdc9294c8773fe691fcbdbf95be5b
Miroslav Sommer <miroslav.sommer@iotics.com>
2021-04-29 16:24:00 +0100

    removed CONTENTHEADER

--- iotics-api/iotics-api-twins/functions.sh
+++ iotics-api/iotics-api-twins/functions.sh
@@ -4,8 +4,6 @@ HOST="https://katacoda.dev.iotics.space/qapi"
 
 RESOLVER="https://did.stg.iotics.com"
 
-CONTENTHEADER="-H \"Content-Type: application/json\" -H \"Accept: application/json\""
-
 TOKEN="eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2thdGFjb2RhLmRldi5pb3RpY3Muc3BhY2UvIiwiZXhwIjoxNjI3NDgzMDU3LCJpYXQiOjE2MTk3MDcwMjcsImlzcyI6ImRpZDppb3RpY3M6aW90SHAyWXJ2OTN1VmlrTTN3ajNhUUdwNHdDSmhIVUJWb2QzI2FnZW50LTAiLCJzdWIiOiJkaWQ6aW90aWNzOmlvdE5lMkNUQ2djZzNERmEydHRyMllqVjJVeDV3SzFaRHlCYiJ9.eE09qNtE4xsSPC8CkVtA0DQQKS4zKDfK6av6RciG5Jyt_FfVcnzOw1oqH7kzJzYY6vNtda_rRRq43DAaySXRlg"
 TWIN0="did:iotics:iotJvf8Y5CSXGsUfi16R568TecB7yPtr5UBz"
 TWIN1="did:iotics:iotPWYjp7k8SFT4FoHtPZzveek5zVdC74Myi"

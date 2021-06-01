a1fc71e95d1ed29b31c676a87f0a1236db0ee66d
jimmy-moore <james.moore@iotics.com>
2021-06-01 15:31:04 +0100

    Added first version of spacectl tutorials

---
+++ iotic-spacectl/iotic-spacectl-intro/install.sh
@@ -0,0 +1,13 @@
+# IIIII   OOO   TTTTT  IIIII   CCC    SSS
+#   I    O   O    T      I    C   C  S
+#   I    O   O    T      I    C       SSS
+#   I    O   O    T      I    C   C      S
+# IIIII   OOO     T    IIIII   CCC    SSS
+#
+# Downloading iotic-spacectl...
+#
+curl -L "https://github.com/jimmy-moore/katacoda-scenarios/raw/main/iotic-spacectl/iotic-spacectl-intro/assets/iotic-spacectl" -o iotic-spacectl
+chmod 770 iotic-spacectl
+apt-get install xml2
+#
+echo "iotic-spacectl READY TO PLAY!"
